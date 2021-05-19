81b26e1f687a1e238643b67901299f35341b290d
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-19 21:47:25 +0200

    Back to development version.

--- optmagic/__init__.py
+++ optmagic/__init__.py
@@ -8,7 +8,7 @@ Contact at www.sinclair.bio
 """
 
 # Constants #
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 # Built-in modules #
 import sys, argparse, types, inspect, functools, os.path
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
 # Call setup #
 setup(
     name             = 'optmagic',
-    version          = '1.0.4',
+    version          = '1.0.5',
     description      = 'Automatically make a command line interface from a '
                        'class or function.',
     license          = 'MIT',
